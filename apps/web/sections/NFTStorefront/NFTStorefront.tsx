08165d05c38c68428bacd8f8bfafab244f918574
Neo-Gar <nvatlecov@mail.ru>
2025-02-11 18:31:19 +0200

    Fixed grid mode

--- apps/web/sections/NFTStorefront/NFTStorefront.tsx
+++ apps/web/sections/NFTStorefront/NFTStorefront.tsx
@@ -9,7 +9,7 @@ import {
   SelectTriggerPick,
   SelectValue,
 } from "../../../../packages/sdk/components/shared/Select/Select";
-import { cn } from "@zknoid/sdk/lib/helpers";
+import { cn, formatAddress } from "@zknoid/sdk/lib/helpers";
 import Image from "next/image";
 import { AnyNFTItem, NFTCollectionIDList } from "../../lib/types/nftTypes";
 import NFTDetailsModal from "../../widgets/NFTDetailsModal";
@@ -32,17 +32,20 @@ function NFTItem({
   return (
     <div
       className={cn(
-        "bg-bg-grey lg:!rounded-[0.521vw] flex flex-col overflow-hidden",
+        "bg-bg-grey lg:!rounded-[0.521vw] overflow-hidden",
         gridMode == 1
-          ? "w-full h-[2.865vw]"
+          ? "w-full h-[2.865vw] grid grid-cols-10 pl-[0.26vw] py-[0.26vw] items-center pr-[0.625vw]"
           : gridMode == 4
-            ? "lg:!w-[15.208vw] lg:!h-[22vw]"
-            : "lg:!w-[9.896vw] lg:!h-[16.6vw]",
+            ? "lg:!w-[15.208vw] lg:!h-[22vw] flex flex-col"
+            : "lg:!w-[9.896vw] lg:!h-[16.6vw] flex flex-col",
       )}
     >
       <button
         onClick={() => setChoosenID(nft.id)}
-        className={"hover:opacity-80 overflow-hidden rounded-[0.26vw]"}
+        className={cn(
+          "hover:opacity-80 overflow-hidden rounded-[0.26vw]",
+          gridMode == 1 ? "w-[2.344vw] h-[2.344vw]" : "w-full h-full",
+        )}
       >
         <Image
           src={`https://res.cloudinary.com/dw4kivbv0/image/upload/w_1000,f_auto,q_auto:best/v1/${nft.imageID}`}
@@ -52,58 +55,120 @@ function NFTItem({
           height={1000}
         />
       </button>
-      <div className={"p-[0.521vw] flex flex-col gap-[0.521vw]"}>
-        <span
-          className={cn(
-            "font-museo text-foreground font-bold leading-[100%]",
-            gridMode == 4 ? "text-[1.25vw]" : "text-[0.833vw]",
-          )}
-        >
-          {nft.collectionID.charAt(0).toUpperCase() + nft.collectionID.slice(1)}{" "}
-          {nft.id < 100 ? "00" + nft.id : "0" + nft.id}
-        </span>
-        <span
-          className={cn(
-            "text-foreground",
-            gridMode == 4
-              ? "text-[1.042vw] leading-[100%] font-museo font-medium"
-              : gridMode == 6
-                ? "text-[0.729vw] leading-[100%] font-museo font-medium"
-                : "text-[0.833vw] leading-[110%] font-plexsans",
-          )}
-        >
-          {nft.price} MINA
-        </span>
-        {nft.isMinted ? (
-          <div
+      {gridMode == 1 ? (
+        <>
+          <span
             className={
-              "py-[0.417vw] flex flex-col items-start justify-center w-full"
+              "col-span-3 font-museo text-foreground font-bold leading-[100%] text-[0.833vw]"
             }
           >
+            {nft.collectionID.charAt(0).toUpperCase() +
+              nft.collectionID.slice(1)}{" "}
+            {nft.id < 100 ? "00" + nft.id : "0" + nft.id}
+          </span>
+          <span
+            className={
+              "col-span-2 text-foreground text-[0.833vw] leading-[110%] font-plexsans"
+            }
+          >
+            {nft.price} MINA
+          </span>
+          {nft.owner ? (
             <span
               className={
-                "lg:!text-[0.833vw] font-plexsans leading-[110%] text-left-accent"
+                "col-span-2 text-foreground text-[0.833vw] leading-[110%] font-plexsans"
+              }
+            >
+              {formatAddress(nft.owner)}
+            </span>
+          ) : (
+            <span
+              className={
+                "col-span-2 text-foreground text-[0.833vw] leading-[110%] font-plexsans"
+              }
+            >
+              Avalible for Mint
+            </span>
+          )}
+          {nft.isMinted ? (
+            <span
+              className={
+                "col-span-2 text-end text-left-accent text-[0.833vw] leading-[110%] font-plexsans"
               }
             >
               Already Minted
             </span>
-          </div>
-        ) : (
-          <div
-            className={
-              "bg-left-accent cursor-pointer hover:opacity-80 py-[0.417vw] rounded-[0.26vw] flex flex-col items-center justify-center w-full"
-            }
-          >
-            <span
+          ) : (
+            <div
               className={
-                "font-museo text-bg-grey font-medium text-[1.042vw] leading-[100%]"
+                "col-span-2 bg-left-accent cursor-pointer hover:opacity-80 py-[0.313vw] rounded-[0.26vw] flex flex-col items-center justify-center w-full"
               }
             >
-              Buy
-            </span>
-          </div>
-        )}
-      </div>
+              <span
+                className={
+                  "font-museo text-bg-grey font-medium text-[0.833vw] leading-[100%]"
+                }
+              >
+                Buy
+              </span>
+            </div>
+          )}
+        </>
+      ) : (
+        <div className={"p-[0.521vw] flex flex-col gap-[0.521vw]"}>
+          <span
+            className={cn(
+              "font-museo text-foreground font-bold leading-[100%]",
+              gridMode == 4 ? "text-[1.25vw]" : "text-[0.833vw]",
+            )}
+          >
+            {nft.collectionID.charAt(0).toUpperCase() +
+              nft.collectionID.slice(1)}{" "}
+            {nft.id < 100 ? "00" + nft.id : "0" + nft.id}
+          </span>
+          <span
+            className={cn(
+              "text-foreground",
+              gridMode == 4
+                ? "text-[1.042vw] leading-[100%] font-museo font-medium"
+                : gridMode == 6
+                  ? "text-[0.729vw] leading-[100%] font-museo font-medium"
+                  : "text-[0.833vw] leading-[110%] font-plexsans",
+            )}
+          >
+            {nft.price} MINA
+          </span>
+          {nft.isMinted ? (
+            <div
+              className={
+                "py-[0.417vw] flex flex-col items-start justify-center w-full"
+              }
+            >
+              <span
+                className={
+                  "lg:!text-[0.833vw] font-plexsans leading-[110%] text-left-accent"
+                }
+              >
+                Already Minted
+              </span>
+            </div>
+          ) : (
+            <div
+              className={
+                "bg-left-accent cursor-pointer hover:opacity-80 py-[0.417vw] rounded-[0.26vw] flex flex-col items-center justify-center w-full"
+              }
+            >
+              <span
+                className={
+                  "font-museo text-bg-grey font-medium text-[1.042vw] leading-[100%]"
+                }
+              >
+                Buy
+              </span>
+            </div>
+          )}
+        </div>
+      )}
     </div>
   );
 }
@@ -366,6 +431,35 @@ export default function NFTStorefront({
               : "grid-cols-6",
         )}
       >
+        {gridMode == 1 && (
+          <div
+            className={
+              "grid grid-cols-10 border-b border-[#373737] py-[0.521vw]"
+            }
+          >
+            <span
+              className={
+                "col-start-2 col-span-3 font-plexsans leading-[110%] text-[0.833vw] text-foreground"
+              }
+            >
+              Item
+            </span>
+            <span
+              className={
+                "col-span-2 font-plexsans leading-[110%] text-[0.833vw] text-foreground"
+              }
+            >
+              Price
+            </span>
+            <span
+              className={
+                "col-span-2 font-plexsans leading-[110%] text-[0.833vw] text-foreground"
+              }
+            >
+              Owner
+            </span>
+          </div>
+        )}
         {collectionItems.map((item, index) => (
           <NFTItem
             key={index}
